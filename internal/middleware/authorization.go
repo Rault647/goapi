0c0a4d7b3811b8661e65a1a4f3109b15b1413346
Rault647 <ultramarine362@gmail.com>
2026-05-29 15:00:40 -0600

    Fix: Correct typo in struct name

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -33,7 +33,7 @@ func Authorization(next http.Handler) http.Handler {
 			return
 		}
 
-		var loginDetails *tools.loginDetails
+		var loginDetails *tools.LoginDetails
 		loginDetails = (*database).GetUserLoginDetails(username)
 
 		// If client wasn't found with the username or the token doesn't match.
